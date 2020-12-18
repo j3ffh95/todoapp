6dc845abc0171a5571f58ad13800b9144eaf7e3f
j3ffh95 <j3ffh95@gmail.com>
2020-12-18 12:48:54 -0500

    Made the edit me btn to have a click listener

--- public/browser.js
+++ public/browser.js
@@ -1,1 +1,6 @@
-console.log("he");
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("edit-me")) {
+    let userInput = prompt("Enter your desire new text");
+    console.log(userInput);
+  }
+});
